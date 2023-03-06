64e5bceb53d669ce6ee4c4b9586dd5b49a99c7f4
Louis Kedziora <louie_101@live.ca>
2023-03-06 12:18:57 -0700

    Intial branch commit with only DB url changed

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,3 +1,5 @@
+const mongoPassword = process.env.MONGOPASS;
+
 module.exports = {
-  url: "mongodb://localhost:27017/playersheet_db",
+  url: "mongodb+srv://louis-kedziora:"+ mongoPassword +"@cluster0.zladf5u.mongodb.net/playersheetDB",
 };
