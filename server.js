a21bc9abac888a2404babe3c3b94fe48ac4d3494
Louis Kedziora <louie_101@live.ca>
2023-03-06 13:21:50 -0700

    updated google fonts import and directory paths

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose
 require("./src/routes/charactersRoute")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4000;
+let PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
