a59a8742846a62547dade6f2db6daeab2b09da8b
Louis Kedziora <louie_101@live.ca>
2023-03-06 14:18:26 -0700

    changed urls to point to DB server

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const serverURL = process.env.serverURL;
+
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api/characters/get"
+  baseURL: serverURL +"/api/characters/get"
 });
 
 export default instance;
