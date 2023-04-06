5b03a68700cfbade5c3916bb5fa4e29093aecf42
Louis Kedziora <louie_101@live.ca>
2023-04-06 11:00:59 -0700

    Fixed warnings they break deployment

--- src/components/partials/CharacterAppBar.jsx
+++ src/components/partials/CharacterAppBar.jsx
@@ -3,10 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import ShieldSharpIcon from "@mui/icons-material/ShieldSharp";
 
@@ -30,7 +27,6 @@ const sheetLinks = {
 };
 
 export const CharacterAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
   const [character, setCharacter] = useState({});
   const [isFetched, setIsFetched] = useState(false);
 
@@ -38,14 +34,6 @@ export const CharacterAppBar = () => {
     setCharacter(JSON.parse(sessionStorage.getItem("currentCharacter")));
     setIsFetched(true);
   }, []);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   return (
     <AppBar
@@ -85,36 +73,6 @@ export const CharacterAppBar = () => {
             >
               {character.name}
             </Typography>
-            {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              ></Menu>
-            </Box> */}
             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {sheetLabels.map((sheet, index) => (
                 <NavLink
