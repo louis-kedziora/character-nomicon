85c9dc1754ae53c5f6c2493329e68d6c6695417d
Louis Kedziora <louie_101@live.ca>
2023-05-08 16:19:42 -0700

    Refactored characterController.js to conform to mongoose v7

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-
-const serverURL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+//Old url config
+// const serverURL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+const serverURL = "nodejs-example-express-rds.eba-kxz2g5au.ca-central-1.elasticbeanstalk.com"
 
 const instance = axios.create({
   baseURL: serverURL +"/api/characters/get"
