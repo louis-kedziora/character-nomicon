7d0874b534ef0775bc419885055dc132778c9be3
Louis Kedziora <louie_101@live.ca>
2023-05-11 13:23:57 -0700

    Changing enviroment back to heroku

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,2 +1,2 @@
-// export const serverURL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
-export const serverURL = "http://express-aws-eb.eba-kxz2g5au.ca-central-1.elasticbeanstalk.com/"
+export const serverURL = process.env.REACT_APP_SERVER_URL || "http://localhost:4000";
+// export const serverURL = "http://express-aws-eb.eba-kxz2g5au.ca-central-1.elasticbeanstalk.com"
